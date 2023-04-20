f03c98e7cc60ac41f1f49eff1dc481c745617c45
Shiv Shankar Kumar <105009746+shivshankar-kumar-au46@users.noreply.github.com>
2023-04-20 18:05:05 +0530

    some updated in frontend

--- frontend/src/pages/signin/Signin.js
+++ frontend/src/pages/signin/Signin.js
@@ -8,7 +8,6 @@ import Cookies from "js-cookie";
 
 const User = () => {
   const navigate = useNavigate();
-  //   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
